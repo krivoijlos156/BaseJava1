404a2894fbf6efe860fc9d2a0410e5ffcc82e30f
krivoijlos156 <krivoijlos@gmail.com>
2020-07-08 13:53:45 +0300

    10.4 update DataStreamSerializer

--- src/com/basejava/webapp/ResumeTestData.java
+++ src/com/basejava/webapp/ResumeTestData.java
@@ -11,6 +11,7 @@ public class ResumeTestData {
 
         r.addContact(ContactType.MAIL, "mail1@ya.ru");
         r.addContact(ContactType.PHONE, "11111");
+        r.addContact(ContactType.SKYPE, "skype.skype");
 
         r.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
         r.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
@@ -27,8 +28,8 @@ public class ResumeTestData {
                                 new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
         r.addSection(SectionType.EDUCATION,
                 new OrganizationSection(
-                        new Organization("Institute", null,
-                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
+                        new Organization("Institute", "",
+                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", ""),
                                 new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "student", "IT facultet")),
                         new Organization("Organization12", "http://Organization12.ru")));
         return r;
--- src/com/basejava/webapp/storage/IOStrategy/DataStreamSerializer.java
+++ src/com/basejava/webapp/storage/IOStrategy/DataStreamSerializer.java
@@ -1,10 +1,16 @@
 package com.basejava.webapp.storage.IOStrategy;
 
-import com.basejava.webapp.model.ContactType;
-import com.basejava.webapp.model.Resume;
+import com.basejava.webapp.model.*;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+
+import static com.basejava.webapp.util.DateUtil.of;
 
 public class DataStreamSerializer implements StreamSerializer {
     @Override
@@ -13,13 +19,64 @@ public class DataStreamSerializer implements StreamSerializer {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
             Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
-            }
-            // TODO implements sections
+            ArrayList<Map.Entry<ContactType, String>> contactsList = new ArrayList<>(contacts.entrySet());
+
+            writeCollect(dos, contactsList, (i) -> {
+                dos.writeUTF(contactsList.get(i).getKey().name());
+                dos.writeUTF(contactsList.get(i).getValue());
+            });
+
+            Map<SectionType, Section> sections = r.getSections();
+            ArrayList<Map.Entry<SectionType, Section>> sectionsList = new ArrayList<>(sections.entrySet());
+            writeCollect(dos, sectionsList, i -> {
+                dos.writeUTF(sectionsList.get(i).getKey().name());
+                switch (sectionsList.get(i).getValue().getClass().toString()) {
+                    case "class com.basejava.webapp.model.TextSection":
+                        dos.writeUTF(((TextSection) sectionsList.get(i).getValue()).getContent());
+                        break;
+                    case "class com.basejava.webapp.model.ListSection":
+                        List<String> textList = ((ListSection) sectionsList.get(i).getValue()).getItems();
+                        writeCollect(dos, textList, j -> dos.writeUTF(textList.get(j)));
+                        break;
+                    case "class com.basejava.webapp.model.OrganizationSection":
+                        List<Organization> organizations =
+                                ((OrganizationSection) sectionsList.get(i).getValue()).getOrganizations();
+                        writeCollect(dos, organizations, k -> {
+                            writeLink(dos, organizations.get(k).getHomePage());
+                            List<Organization.Position> positions = organizations.get(k).getPositions();
+                            writeCollect(dos, positions, p -> {
+                                writeDate(dos, positions.get(p).getStartDate());
+                                writeDate(dos, positions.get(p).getEndDate());
+                                dos.writeUTF(positions.get(p).getTitle());
+                                dos.writeUTF(positions.get(p).getDescription());
+                            });
+                        });
+                        break;
+                }
+            });
         }
+    }
+
+    public interface InRotationWrite {
+        void doInRotation(int i) throws IOException;
+    }
+
+    private <T> void writeCollect(DataOutputStream dos, Collection<T> collection, InRotationWrite doInRotation) throws IOException {
+        int colSize = collection.size();
+        dos.writeInt(colSize);
+        for (int i = 0; i < colSize; i++) {
+            doInRotation.doInRotation(i);
+        }
+    }
+
+    private void writeLink(DataOutputStream dos, Link link) throws IOException {
+        dos.writeUTF(link.getName());
+        dos.writeUTF(link.getUrl());
+    }
+
+    private void writeDate(DataOutputStream dos, LocalDate date) throws IOException {
+        dos.writeInt(date.getYear());
+        dos.writeInt(date.getMonth().getValue());
     }
 
     @Override
@@ -28,12 +85,62 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
-            // TODO implements sections
+            readCollect(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readCollect(dis, () -> {
+                SectionType st = SectionType.valueOf(dis.readUTF());
+                switch (st) {
+                    case OBJECTIVE:
+                    case PERSONAL:
+                        resume.addSection(st, new TextSection(dis.readUTF()));
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        ArrayList<String> textList = new ArrayList<>();
+                        readCollect(dis, () -> textList.add(dis.readUTF()));
+                        resume.addSection(st, new ListSection(textList));
+                        break;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        List<Organization> organizationList = new ArrayList<>();
+                        readCollect(dis, () -> {
+                            List<Organization.Position> positionList = new ArrayList<>();
+                            Link link = readLink(dis);
+                            readCollect(dis, () -> positionList.add(new Organization.Position(
+                                    readDate(dis),
+                                    readDate(dis),
+                                    dis.readUTF(),
+                                    dis.readUTF()
+                            )));
+                            organizationList.add(new Organization(link, positionList));
+                        });
+                        OrganizationSection organizationSection = new OrganizationSection(organizationList);
+                        resume.addSection(st, organizationSection);
+                }
+            });
             return resume;
         }
+    }
+
+    public interface InRotationRead {
+        void doInRotation() throws IOException;
+    }
+
+    private void readCollect(DataInputStream dis, InRotationRead doInRotation) throws IOException {
+        int colSize = dis.readInt();
+        for (int i = 0; i < colSize; i++) {
+            doInRotation.doInRotation();
+        }
+    }
+
+    private Link readLink(DataInputStream dis) throws IOException {
+        String name = dis.readUTF();
+        String url = dis.readUTF();
+        return new Link(name, url);
+    }
+
+    private LocalDate readDate(DataInputStream dis) throws IOException {
+        int year = dis.readInt();
+        Month month = Month.of(dis.readInt());
+        return of(year, month);
     }
 }

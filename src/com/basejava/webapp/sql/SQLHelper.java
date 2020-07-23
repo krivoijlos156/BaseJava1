4bccfb77ce84c0bc1faeb1bf6367a8af6aaeb190
krivoijlos156 <krivoijlos@gmail.com>
2020-07-23 13:57:34 +0300

    14.0 create ExceptionUtil, SqlTransaction (add contact) update SQLStorage, SQLHelper

--- src/com/basejava/webapp/sql/SQLHelper.java
+++ src/com/basejava/webapp/sql/SQLHelper.java
@@ -19,13 +19,25 @@ public class SQLHelper {
              PreparedStatement ps = conn.prepareStatement(sql)) {
             return sqlExecute.sqlExecute(ps);
         } catch (SQLException e) {
-            if (e.getErrorCode() == 0) {
-                throw new ExistStorageException(null);
-            }
-            throw new StorageException(e);
+            throw ExceptionUtil.convertException(e);
         }
     }
 
+    public <T> T transactionalExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T res = executor.execute(conn);
+                conn.commit();
+                return res;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw ExceptionUtil.convertException(e);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
     public interface SqlExecuter<T> {
         T sqlExecute(PreparedStatement ps) throws SQLException;
     }
--- src/com/basejava/webapp/storage/SQLStorage.java
+++ src/com/basejava/webapp/storage/SQLStorage.java
@@ -1,6 +1,7 @@
 package com.basejava.webapp.storage;
 
 import com.basejava.webapp.exception.NotExistStorageException;
+import com.basejava.webapp.model.ContactType;
 import com.basejava.webapp.model.Resume;
 import com.basejava.webapp.sql.SQLHelper;
 
@@ -10,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 
@@ -37,18 +39,45 @@ public class SQLStorage implements Storage {
     @Override
     public void save(Resume resume) {
         LOG.info("Save " + resume);
-        sqlHelper.execute("INSERT INTO resume (uuid, full_name) VALUES (?,?)",
-                ps -> setParamAndExecute(ps, resume, 2, 1, false));
+        sqlHelper.transactionalExecute(conn -> {
+                    try (PreparedStatement ps = conn.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)")) {
+                        ps.setString(1, resume.getUuid());
+                        ps.setString(2, resume.getFullName());
+                        ps.execute();
+                    }
+                    try (PreparedStatement ps = conn.prepareStatement("INSERT INTO contact (resume_uuid, type, value) VALUES (?,?,?)")) {
+                        for (Map.Entry<ContactType, String> e : resume.getContacts().entrySet()) {
+                            ps.setString(1, resume.getUuid());
+                            ps.setString(2, e.getKey().name());
+                            ps.setString(3, e.getValue());
+                            ps.addBatch();
+                        }
+                        ps.executeBatch();
+                    }
+                    return null;
+                }
+        );
     }
 
     @Override
     public Resume get(String uuid) {
         LOG.info("Get " + uuid);
-        return sqlHelper.execute("SELECT * FROM resume r WHERE r.uuid =?", (ps) -> {
-            ps.setString(1, uuid);
-            ResultSet rs = ifNull(uuid, ps);
-            return new Resume(uuid, rs.getString("full_name"));
-        });
+        return sqlHelper.execute("" +
+                        "    SELECT * FROM resume r " +
+                        " LEFT JOIN contact c " +
+                        "        ON r.uuid = c.resume_uuid " +
+                        "     WHERE r.uuid =? ",
+                (ps) -> {
+                    ps.setString(1, uuid);
+                    ResultSet rs = ifNull(uuid, ps);
+                    Resume r = new Resume(uuid, rs.getString("full_name"));
+                    do {
+                        String value = rs.getString("value");
+                        ContactType type = ContactType.valueOf(rs.getString("type"));
+                        r.addContact(type, value);
+                    } while (rs.next());
+                    return r;
+                });
     }
 
     @Override

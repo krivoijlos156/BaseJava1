258e91b56262da1e0a95a8bf84ca326843d66dbc
krivoijlos156 <krivoijlos@gmail.com>
2020-08-03 22:13:22 +0300

    16.1 Add Section to web (ResumeServlet)

--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -1,8 +1,7 @@
 package com.basejava.webapp.web;
 
 import com.basejava.webapp.Config;
-import com.basejava.webapp.model.ContactType;
-import com.basejava.webapp.model.Resume;
+import com.basejava.webapp.model.*;
 import com.basejava.webapp.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -11,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -33,6 +32,29 @@ public class ResumeServlet extends HttpServlet {
             String value = request.getParameter(type.name());
             if (value != null && value.trim().length() != 0) {
                 r.addContact(type, value);
+            } else {
+                r.getContacts().remove(type);
+            }
+        }
+        for (SectionType type : SectionType.values()) {
+            String value = request.getParameter(type.name());
+            if (value != null && value.trim().length() != 0) {
+                switch (type) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        r.addSection(type, new TextSection(value));
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        r.addSection(type, new ListSection(Arrays.asList(value.split(", "))));
+                        break;
+                    case EXPERIENCE:
+                        break;
+                    case EDUCATION:
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + type);
+                }
             } else {
                 r.getContacts().remove(type);
             }
